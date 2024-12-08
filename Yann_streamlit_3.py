776df1153d380de78ed2c9052ce007a118c19b4b
Yann-ML <macleod.yann@gmail.com>
2024-12-08 08:52:47 +0100

    Update Yann_streamlit_3.py

--- Yann_streamlit_3.py
+++ Yann_streamlit_3.py
@@ -23,18 +23,12 @@ authenticator = Authenticate(
     30, # Le nombre de jours avant que le cookie expire 
 )
 
+
 authenticator.login()
 
 
-def accueil():
-      st.title("Bienvenu sur le contenu réservé aux utilisateurs connectés")
-
-
 if st.session_state["authentication_status"]:
-    accueil()
-
-    # Le bouton de déconnexion
-    authenticator.logout("Déconnexion")
+    st.title("Bienvenue")
 
 elif st.session_state["authentication_status"] is False:
     st.error("L'username ou le password est/sont incorrect")
@@ -46,14 +40,40 @@ elif st.session_state["authentication_status"] is None:
 from streamlit_option_menu import option_menu
 
 # Création du menu qui va afficher les choix qui se trouvent dans la variable options
-selection = option_menu(
-            menu_title=None,
-            options = ["Accueil", "Photos"]
-        )
+with st.sidebar:
+
+    st.write(f"Bienvenue {lesDonneesDesComptes['usernames']['root']['name']}")
+
+    selection = option_menu(
+                menu_title=None,
+                options=["Accueil", "Photos"]
+            )
+    # Le bouton de déconnexion
+    authenticator.logout("Déconnexion")
 
 # On indique au programme quoi faire en fonction du choix
 if selection == "Accueil":
-    st.write("Bienvenue sur la page d'accueil !")
+
+    st.write("Bienvenue sur ma page !")
+
+    # affichage image accueil
+    st.image("https://cdn-www.konbini.com/files/2024/03/messi-anatomie.jpg?width=3840&quality=75&format=webp")
+
+# page Photo 
 elif selection == "Photos":
+
     st.write("Bienvenue sur mon album photo")
-# ... et ainsi de suite pour les autres pages
+
+    col1, col2, col3 = st.columns(3)
+
+    with col1:
+        st.header("Un chat")
+        st.image("https://www.assuropoil.fr/wp-content/uploads/2023/07/avoir-un-chat-sante.jpg")
+
+    with col2:
+        st.header("Toujours un chat")
+        st.image("https://www.peuple-animal.com/wp-content/uploads/2019/02/1739.800.jpg")
+
+    with col3:
+        st.header("Un autre chat")
+        st.image("https://www.slate.fr/uploads/store/drupal_slate/chat_vener.jpg")
